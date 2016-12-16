cef5edf889077f1ada88b9147696f1cad688fd85
Ruchira Padave <ruchiwohlig@gmail.com>
2016-12-16 15:44:09 +0530

    reward page added

--- www/js/app.js
+++ www/js/app.js
@@ -74,6 +74,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'KpiCtrl'
       }
     }
+  })
+  
+    .state('app.reward', {
+    url: '/reward',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/reward.html',
+        controller: 'RewardCtrl'
+      }
+    }
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/login');
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -34,4 +34,18 @@ $scope.menu=[
    $scope.isGroupShown = function(group) {
      return $scope.shownGroup === group;
    };
+})
+
+.controller('RewardCtrl', function($scope, $stateParams) {
+  // $scope.groups = [];
+   $scope.toggleGroup = function(group) {
+     if ($scope.isGroupShown(group)) {
+       $scope.shownGroup = null;
+     } else {
+       $scope.shownGroup = group;
+     }
+   };
+   $scope.isGroupShown = function(group) {
+     return $scope.shownGroup === group;
+   };
 });
